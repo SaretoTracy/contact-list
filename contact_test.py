f9f46adfb32ebe7e455fbcefd3d54859be76cab3
saretotracy <tracysareto@gmail.com>
2022-02-19 17:52:59 +0300

    tests if we can find a contact object we use the method find_by_number() that takes on a phone number, and returns a contact object.

--- contact_test.py
+++ contact_test.py
@@ -62,6 +62,19 @@ class TestContact(unittest.TestCase): #subclass class that inherits from unittes
             self.new_contact.delete_contact()# Deleting a contact object
             self.assertEqual(len(Contact.contact_list),1)
 
+    def test_find_contact_by_number(self): #fifth test
+        '''
+        test to check if we can find a contact by phone number and display information
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+        test_contact.save_contact()
+
+        found_contact = Contact.find_by_number("0711223344")
+
+        self.assertEqual(found_contact.email,test_contact.email)       
+
     
 
 if __name__ ==  '__main__':

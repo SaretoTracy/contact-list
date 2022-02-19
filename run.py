6d1e6e3d1dbb574c93de04179841104546326a2a
saretotracy <tracysareto@gmail.com>
2022-02-19 20:21:41 +0300

    create a function called create_contact(), that takes in four arguments

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 from contact import Contact
+# functions that implement the behaviours we have created
+def create_contact(fname,lname,phone,email):
+    '''
+    Function to create a new contact
+    '''
+    new_contact = Contact(fname,lname,phone,email)
+    return new_contact

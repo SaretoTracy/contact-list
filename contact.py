10811a4bb1b4426f7513f3adde365ae666f46a84
saretotracy <tracysareto@gmail.com>
2022-02-19 17:14:01 +0300

    create contact object and appends it to our contact-list list.

--- contact.py
+++ contact.py
@@ -4,7 +4,14 @@ class Contact:
     """
 
     contact_list = [] # Empty contact list
+ # Init method up here
+    def save_contact(self):
 
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Contact.contact_list.append(self)
     def __init__(self,first_name,last_name,number,email):
 
       # docstring removed for simplicity
